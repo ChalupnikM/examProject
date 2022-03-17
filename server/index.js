5e94a9458507de7664c0bae2cc9eedfc6a98182a
ChalupnikM <chalupnik.michal1@gmail.com>
2022-03-17 20:07:31 +0100

    make it better

--- server/index.js
+++ server/index.js
@@ -3,19 +3,31 @@ const app = express();
 const cors = require('cors');
 const axios = require('axios');
 
-app.use(cors()); 
+app.use(cors());
+const path = require('path');
 
 app.get('*', cors(), (req, res, next) => {
-    console.log("'/test' call", req.path);
-    axios.get(`https://unsplash.com/nautocomplete${req.path}`)
-      .then(response => res.json(response.data))
-      .catch(err => next(err));
-  })
+  console.log("'/test' call", req.path);
+  axios.get(`https://unsplash.com/nautocomplete${req.path}`)
+    .then(response => res.json(response.data))
+    .catch(err => next(err));
+})
 
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('build'))
+  app.get('*', (req, res) => {
+    req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  })
+}
 // app.listen(3000, function () {
 //   console.log('Listening on port 3000');
 // });
 
-app.use(express.static(path.join(__dirname, 'build')));
+//app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, (err) => {
+  if (err) return console.log(err);
+  console.log('Server running on port: ', port)
+});
